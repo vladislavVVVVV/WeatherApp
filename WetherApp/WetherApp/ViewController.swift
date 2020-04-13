fdda2f7e700c60301cd8e852334581e7feeef6a6
vladislavVVVVV <vlad.evnevich@icloud.com>
2020-04-13 15:51:00 +0300

    first commit

---
+++ WetherApp/WetherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WetherApp
+//
+//  Created by Uladzislau Yaunevich on 4/13/20.
+//  Copyright © 2020 UladzislauCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
