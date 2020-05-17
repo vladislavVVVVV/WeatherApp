6cd0b389e7df92e2ba511b9f7486b76f371fabdd
vladislavVVVVV <vlad.evnevich@icloud.com>
2020-05-17 18:08:33 +0300

    changed xib views to programaticly

---
+++ WeatherApp/WeatherApp/Extentions/UIView+addSubviews.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+addSubviews.swift
+//  WeatherApp
+//
+//  Created by Uladzislau Yaunevich on 5/17/20.
+//  Copyright © 2020 UladzislauCompany. All rights reserved.
+//
+
+import Foundation
